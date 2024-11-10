41531aa4a19e7db0a7e9d86eaad5b21e8d356077
laibasiddique75 <laibasiddique111@gmail.com>
2024-11-10 13:16:51 +0500

    added

--- script.js
+++ script.js
@@ -19,10 +19,27 @@ var _a;
         var education = educationElement.value;
         var experience = experienceElement.value;
         var skills = skillsElement.value;
-        // picture elements
-        var profilePictureFile = (_a = profilePicture.files) === null || _a === void 0 ? void 0 : _a[0];
-        var profilePictureUrl = profilePictureFile ? URL.createObjectURL(profilePictureFile) : "";
-        var username = usernameElement.value;
+        //  picture elements
+         var profilePictureFile = (_a = profilePicture.files) === null || _a === void 0 ? void 0 : _a[0];
+         var profilePictureUrl = profilePictureFile ? URL.createObjectURL(profilePictureFile) : "";
+         var username = usernameElement.value;
+
+
+
+
+
+        // document.getElementById('profile-picture').addEventListener('change', function () {
+        //     var profilePictureFile = this.files?.[0];
+        //     var profilePictureUrl = profilePictureFile ? URL.createObjectURL(profilePictureFile) : "";
+        
+        //     // Display the image preview
+        //     document.getElementById('profileImage').src = profilePictureUrl;
+        
+        //     // Get username if necessary
+        //     var username = document.getElementById('username').value;
+        // });
+        
+
         var uniquePath = "resumes/".concat(username.replace(/\ s+/g, "_"), "_cv.html");
         // create resume output
         var resumeOutput = "<h2>Resume</h2>\n\n    ".concat(profilePictureUrl ? "<img src = \"".concat(profilePictureUrl, "\" alt =\"ProfilePicture\" class = \"profilePicture\">") : '', "\n    <p><strong>Name:</strong> ").concat(name_1, " </p>\n    <p><strong>Email:</strong> ").concat(email, " </p>\n    <p><strong>Phone:</strong> ").concat(phone, " </p>\n    \n    <h3>Education</h3>\n    <p>").concat(education, "</p> \n    \n    <h3>Experience</h3>\n    <p> ").concat(experience, " </p>\n    \n    <h3>Skills</h3>\n    <p> ").concat(skills, " </p>\n    \n    \n    ");
