cac02bf848cf5153c9a2f58e77de5147b7079eb6
laibasiddique75 <laibasiddique111@gmail.com>
2024-11-10 22:50:09 +0500

    added

--- script.js
+++ script.js
@@ -1,62 +1,45 @@
-// listing element
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __generator = (this && this.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (g && (g = 0, op[0] && (_ = 0)), _) try {
+            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [op[0] & 2, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
+};
 var _a;
-// document.getElementById("resume-form")?.addEventListener("submit",function(event) {
-//     event.preventDefault();
-//     const profilePicture = document.getElementById("profile-picture") as HTMLInputElement;
-//     // type assertion 
-//     const nameElement =document.getElementById("name") as HTMLInputElement;
-//     const emailElement =document.getElementById("email") as HTMLInputElement;
-//     const phoneElement =document.getElementById("phone") as HTMLInputElement;
-//     const educationElement =document.getElementById("education") as HTMLInputElement;
-//     const experienceElement =document.getElementById("experience") as HTMLInputElement;
-//     const skillsElement =document.getElementById("skills") as HTMLInputElement;
-//     const usernameElement = document.getElementById("user-name") as HTMLInputElement;
-//     if(profilePicture && nameElement && emailElement && phoneElement && educationElement && experienceElement && skillsElement &&  usernameElement){
-//         const name = nameElement.value ;
-//         const email = emailElement.value ;
-//         const phone = phoneElement.value ;
-//         const education = educationElement.value ;
-//         const experience = experienceElement.value ;
-//         const skills = skillsElement.value ;
-//         // picture elements
-//         const profilePictureFile = profilePicture.files?.[0];
-//         const profilePictureUrl = profilePictureFile?URL.createObjectURL(profilePictureFile) : "";
-//         const username = usernameElement.value;
-//         const uniquePath =`resumes/${username.replace(/\ s+/g, "_")}_cv.html`
-//     // create resume output
-//     const resumeOutput = `<h2>Resume</h2>
-//     ${profilePictureUrl ?`<img src = "${profilePictureUrl}" alt ="ProfilePicture" class = "profilePicture">`:''}
-//     <p><strong>Name:</strong> ${name} </p>
-//     <p><strong>Email:</strong> ${email} </p>
-//     <p><strong>Phone:</strong> ${phone} </p>
-//     <h3>Education</h3>
-//     <p>${education}</p> 
-//     <h3>Experience</h3>
-//     <p> ${experience} </p>
-//     <h3>Skills</h3>
-//     <p> ${skills} </p>
-//     `;
-//     const downloadLink = document.createElement("a")
-// downloadLink.href = "data:text/html;charset = utf-8," +encodeURIComponent(resumeOutput)
-// downloadLink.download = uniquePath;
-// downloadLink.textContent = "Download Your 2024 Resume";
-//     // display resume output
-//     const resumeOutputElement = document.getElementById("resume-output")
-//     if(resumeOutputElement){
-//     resumeOutputElement.innerHTML = resumeOutput
-//     resumeOutputElement.appendChild(downloadLink)
-//     }else{
-//         console.error("The resume output elements are missing");
-//     }
-//      } else{
-//         console.error("One or more output elements are missing")
-//     }
-//     }
-//     );
 (_a = document.getElementById("resume-form")) === null || _a === void 0 ? void 0 : _a.addEventListener("submit", function (event) {
+    var _this = this;
     var _a;
     event.preventDefault();
-    var profilePicture = document.getElementById("profile-picture");
+    var profilePictureInput = document.getElementById("profile-picture");
     // type assertion 
     var nameElement = document.getElementById("name");
     var emailElement = document.getElementById("email");
@@ -65,7 +48,7 @@ var _a;
     var experienceElement = document.getElementById("experience");
     var skillsElement = document.getElementById("skills");
     var usernameElement = document.getElementById("user-name");
-    if (profilePicture && nameElement && emailElement && phoneElement && educationElement && experienceElement && skillsElement && usernameElement) {
+    if (profilePictureInput && nameElement && emailElement && phoneElement && educationElement && experienceElement && skillsElement && usernameElement) {
         var name_1 = nameElement.value;
         var email = emailElement.value;
         var phone = phoneElement.value;
@@ -73,12 +56,12 @@ var _a;
         var experience = experienceElement.value;
         var skills = skillsElement.value;
         // picture elements
-        var profilePictureFile = (_a = profilePicture.files) === null || _a === void 0 ? void 0 : _a[0];
-        var profilePictureUrl = profilePictureFile ? URL.createObjectURL(profilePictureFile) : "";
+        var profilePictureFile = (_a = profilePictureInput.files) === null || _a === void 0 ? void 0 : _a[0];
+        var profilePictureURL = profilePictureFile ? URL.createObjectURL(profilePictureFile) : "";
         var username = usernameElement.value;
         var uniquePath = "resumes/".concat(username.replace(/\ s+/g, "_"), "_cv.html");
         // create resume output
-        var resumeOutput = "<h2>Resume</h2>\n\n    ".concat(profilePictureUrl ? "<img src = \"".concat(profilePictureUrl, "\" alt =\"ProfilePicture\" class = \"profilePicture\">") : '', "\n    <p><strong>Name:</strong> ").concat(name_1, " </p>\n    <p><strong>Email:</strong> ").concat(email, " </p>\n    <p><strong>Phone:</strong> ").concat(phone, " </p>\n    \n    <h3>Education</h3>\n    <p>").concat(education, "</p> \n    \n    <h3>Experience</h3>\n    <p> ").concat(experience, " </p>\n    \n    <h3>Skills</h3>\n    <p> ").concat(skills, " </p>\n    \n    \n    ");
+        var resumeOutput = "<h2>Resume</h2>\n\n    ".concat(profilePictureURL ? "<img src = \"".concat(profilePictureURL, "\" alt =\"ProfilePicture\" class = \"profilePicture\">") : '', "\n    <p><strong>Name:</strong> ").concat(name_1, " </p>\n    <p><strong>Email:</strong> ").concat(email, " </p>\n    <p><strong>Phone:</strong> ").concat(phone, " </p>\n    \n    <h3>Education</h3>\n    <p>").concat(education, "</p> \n    \n    <h3>Experience</h3>\n    <p> ").concat(experience, " </p>\n    \n    <h3>Skills</h3>\n    <p> ").concat(skills, " </p>\n    \n    \n    ");
         var downloadLink = document.createElement("a");
         downloadLink.href = "data:text/html;charset = utf-8," + encodeURIComponent(resumeOutput);
         downloadLink.download = uniquePath;
@@ -87,7 +70,49 @@ var _a;
         var resumeOutputElement = document.getElementById("resume-output");
         if (resumeOutputElement) {
             resumeOutputElement.innerHTML = resumeOutput;
-            resumeOutputElement.appendChild(downloadLink);
+            resumeOutputElement.classList.remove('hidden');
+            // create conntainer for button
+            var buttonContainer = document.createElement('div');
+            buttonContainer.id = "buttonsContainer";
+            resumeOutputElement.appendChild(buttonContainer);
+            //add download pdf button
+            // const downloadButton = document.createElement('button');
+            // downloadButton.textContent = "Download as PDF";
+            // downloadButton.addEventListener("click " ,() =>{
+            //     window.print();
+            // });
+            // buttonContainer.appendChild(downloadButton);
+            var downloadButton = document.createElement('button');
+            downloadButton.textContent = "Download as PDF";
+            downloadButton.addEventListener("click", function () {
+                window.print();
+            });
+            buttonContainer.appendChild(downloadButton);
+            //ADD shareable link 
+            var shareLinkButton = document.createElement("button");
+            shareLinkButton.textContent = "copy shreable link";
+            shareLinkButton.addEventListener("click", function () { return __awaiter(_this, void 0, void 0, function () {
+                var shareableLink, err_1;
+                return __generator(this, function (_a) {
+                    switch (_a.label) {
+                        case 0:
+                            _a.trys.push([0, 2, , 3]);
+                            shareableLink = "https://yourdomain.com/".concat(name_1.replace(/\s+/g, "_"), "_cv.html");
+                            return [4 /*yield*/, navigator.clipboard.writeText(shareableLink)];
+                        case 1:
+                            _a.sent();
+                            alert("shreable link copy to clipboard");
+                            return [3 /*break*/, 3];
+                        case 2:
+                            err_1 = _a.sent();
+                            console.log("failed to copy link:", err_1);
+                            alert("ailed to copy link to clipboard.please try again");
+                            return [3 /*break*/, 3];
+                        case 3: return [2 /*return*/];
+                    }
+                });
+            }); });
+            buttonContainer.appendChild(shareLinkButton);
         }
         else {
             console.error("The resume output elements are missing");
--- script.ts
+++ script.ts
@@ -1,115 +1,11 @@
 
 
 
-// listing element
-
-// document.getElementById("resume-form")?.addEventListener("submit",function(event) {
-
-//     event.preventDefault();
-    
-//     const profilePicture = document.getElementById("profile-picture") as HTMLInputElement;
-
-//     // type assertion 
-    
-//     const nameElement =document.getElementById("name") as HTMLInputElement;
-    
-//     const emailElement =document.getElementById("email") as HTMLInputElement;
-    
-//     const phoneElement =document.getElementById("phone") as HTMLInputElement;
-    
-//     const educationElement =document.getElementById("education") as HTMLInputElement;
-    
-//     const experienceElement =document.getElementById("experience") as HTMLInputElement;
-    
-//     const skillsElement =document.getElementById("skills") as HTMLInputElement;
-
-
-//     const usernameElement = document.getElementById("user-name") as HTMLInputElement;
-    
-//     if(profilePicture && nameElement && emailElement && phoneElement && educationElement && experienceElement && skillsElement &&  usernameElement){
-//         const name = nameElement.value ;
-//         const email = emailElement.value ;
-//         const phone = phoneElement.value ;
-//         const education = educationElement.value ;
-//         const experience = experienceElement.value ;
-//         const skills = skillsElement.value ;
-    
-
-
-
-//         // picture elements
-    
-//         const profilePictureFile = profilePicture.files?.[0];
-//         const profilePictureUrl = profilePictureFile?URL.createObjectURL(profilePictureFile) : "";
-    
-
-    
-//         const username = usernameElement.value;
-
-//         const uniquePath =`resumes/${username.replace(/\ s+/g, "_")}_cv.html`
-    
-//     // create resume output
-    
-//     const resumeOutput = `<h2>Resume</h2>
-
-//     ${profilePictureUrl ?`<img src = "${profilePictureUrl}" alt ="ProfilePicture" class = "profilePicture">`:''}
-//     <p><strong>Name:</strong> ${name} </p>
-//     <p><strong>Email:</strong> ${email} </p>
-//     <p><strong>Phone:</strong> ${phone} </p>
-    
-//     <h3>Education</h3>
-//     <p>${education}</p> 
-    
-//     <h3>Experience</h3>
-//     <p> ${experience} </p>
-    
-//     <h3>Skills</h3>
-//     <p> ${skills} </p>
-    
-    
-//     `;
-
-
-
-//     const downloadLink = document.createElement("a")
-// downloadLink.href = "data:text/html;charset = utf-8," +encodeURIComponent(resumeOutput)
-// downloadLink.download = uniquePath;
-// downloadLink.textContent = "Download Your 2024 Resume";
-    
-    
-//     // display resume output
-    
-//     const resumeOutputElement = document.getElementById("resume-output")
-//     if(resumeOutputElement){
-//     resumeOutputElement.innerHTML = resumeOutput
-
-
-
-//     resumeOutputElement.appendChild(downloadLink)
-    
-//     }else{
-//         console.error("The resume output elements are missing");
-        
-//     }
-//      } else{
-    
-//         console.error("One or more output elements are missing")
-//     }
-    
-//     }
-//     );
-
-
-
-
-
-
-
 document.getElementById("resume-form")?.addEventListener("submit",function(event) {
 
     event.preventDefault();
     
-    const profilePicture = document.getElementById("profile-picture") as HTMLInputElement;
+    const profilePictureInput = document.getElementById("profile-picture") as HTMLInputElement;
 
     // type assertion 
     
@@ -128,7 +24,12 @@ document.getElementById("resume-form")?.addEventListener("submit",function(event
 
     const usernameElement = document.getElementById("user-name") as HTMLInputElement;
     
-    if(profilePicture && nameElement && emailElement && phoneElement && educationElement && experienceElement && skillsElement &&  usernameElement){
+    if(profilePictureInput && nameElement && emailElement && phoneElement && educationElement && experienceElement && skillsElement &&  usernameElement)
+        
+        
+        
+    
+        {
         const name = nameElement.value ;
         const email = emailElement.value ;
         const phone = phoneElement.value ;
@@ -141,8 +42,8 @@ document.getElementById("resume-form")?.addEventListener("submit",function(event
 
         // picture elements
     
-        const profilePictureFile = profilePicture.files?.[0];
-        const profilePictureUrl = profilePictureFile?URL.createObjectURL(profilePictureFile) : "";
+        const profilePictureFile = profilePictureInput.files?.[0];
+        const profilePictureURL = profilePictureFile?URL.createObjectURL(profilePictureFile) : "";
     
 
     
@@ -154,7 +55,7 @@ document.getElementById("resume-form")?.addEventListener("submit",function(event
     
     const resumeOutput = `<h2>Resume</h2>
 
-    ${profilePictureUrl ?`<img src = "${profilePictureUrl}" alt ="ProfilePicture" class = "profilePicture">`:''}
+    ${profilePictureURL ?`<img src = "${profilePictureURL}" alt ="ProfilePicture" class = "profilePicture">`:''}
     <p><strong>Name:</strong> ${name} </p>
     <p><strong>Email:</strong> ${email} </p>
     <p><strong>Phone:</strong> ${phone} </p>
@@ -183,11 +84,55 @@ downloadLink.textContent = "Download Your 2024 Resume";
     
     const resumeOutputElement = document.getElementById("resume-output")
     if(resumeOutputElement){
-    resumeOutputElement.innerHTML = resumeOutput
+    resumeOutputElement.innerHTML = resumeOutput;
+
+    resumeOutputElement.classList.remove('hidden');
+    // create conntainer for button
+
+    const buttonContainer = document.createElement('div');
+    buttonContainer.id = "buttonsContainer";
+    resumeOutputElement.appendChild(buttonContainer);
+
+    //add download pdf button
+
+    // const downloadButton = document.createElement('button');
+    // downloadButton.textContent = "Download as PDF";
+    // downloadButton.addEventListener("click " ,() =>{
+    //     window.print();
+    // });
+    // buttonContainer.appendChild(downloadButton);
 
 
+    const downloadButton = document.createElement('button');
+downloadButton.textContent = "Download as PDF";
+downloadButton.addEventListener("click", () => {
+    window.print();
+});
+buttonContainer.appendChild(downloadButton);
 
-    resumeOutputElement.appendChild(downloadLink)
+
+    //ADD shareable link 
+
+    const shareLinkButton = document.createElement("button");
+    shareLinkButton.textContent = "copy shreable link";
+    shareLinkButton.addEventListener("click",async()=>{
+        try{
+            const shareableLink =`https://yourdomain.com/${name.replace(
+                /\s+/g,
+                "_"
+            )}_cv.html`;
+
+            await navigator.clipboard.writeText(shareableLink);
+            alert("shreable link copy to clipboard");
+        }
+        catch(err){
+
+            console.log("failed to copy link:" ,err );
+            alert("ailed to copy link to clipboard.please try again");
+            
+        }
+    });
+    buttonContainer.appendChild(shareLinkButton);
     
     }else{
         console.error("The resume output elements are missing");
